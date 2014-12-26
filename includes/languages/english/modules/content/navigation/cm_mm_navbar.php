563859288d9786ad857a44393c2fffb024528460
clustersolutions <clustersolutions@users.noreply.github.com>
2014-12-25 20:43:34 -0800

    Added Yamm to Navbar. Cluster Solutions.

---
+++ includes/languages/english/modules/content/navigation/cm_mm_navbar.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2014 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_MM_NAVBAR_TITLE', 'Navigation Bar With Megamenu');
+  define('MODULE_CONTENT_MM_NAVBAR_DESCRIPTION', 'Show the Navigation Bar with Megamenu on your site.');
+
